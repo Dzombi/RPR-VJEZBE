9da5074d9cd446293f0d68bd4d3260d6aeb37e8f
Dzombi <benjamin.bazdar@gmail.com>
2023-11-03 19:55:41 +0100

    t3-z1

---
+++ t3-z1/src/main/java/ba/etf/unsa/rpr/Program.java
@@ -0,0 +1,12 @@
+package ba.etf.unsa.rpr;
+
+import java.util.Scanner;
+
+public class Program {
+    public static Scanner scanner=new Scanner(System.in);
+    public static Imenik imenik = new Imenik();
+
+    public static void main(String[] args) {
+        
+    }
+}
